0b6cd0f4e391c18a824e41acf1e2e861b595291d
ironPriest <iron.priest.mih@gmail.com>
2022-09-02 21:00:30 +0300

    #5 braces for delete

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -8,8 +8,8 @@ import {usersRepository} from "../repositories/users-repository";
 export const testingRouter = Router({})
 
 testingRouter.delete('/all-data', async (req, res) => {
-    await bloggersRepository.deleteAll
-    await postsRepository.deleteAll
-    await usersRepository.deleteAll
+    await bloggersRepository.deleteAll()
+    await postsRepository.deleteAll()
+    await usersRepository.deleteAll()
     res.sendStatus(204)
 })
